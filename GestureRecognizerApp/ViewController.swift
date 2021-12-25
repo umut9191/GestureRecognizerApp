93c0ac27f48a6ba16fe40808762b1ac0ac3a40df
umut9191 <umutsurmeli91@gmail.com>
2021-12-25 16:59:39 +0300

    Assets files added to project and codes added about gesture recognizer

--- GestureRecognizerApp/ViewController.swift
+++ GestureRecognizerApp/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imageView.isUserInteractionEnabled=true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changePic))
+        
+        imageView.addGestureRecognizer(gestureRecognizer)
     }
-
+    @objc func changePic(){
+        if myLabel.text=="metallica1" {
+            imageView.image = UIImage(named: "metallica2")
+            myLabel.text="metallica2"
+        }else{
+            imageView.image = UIImage(named: "metallica1")
+            myLabel.text="metallica1"
+        }
+       
+    }
 
 }
 
